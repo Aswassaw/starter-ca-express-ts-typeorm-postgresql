4b74b8eac3108590752d460d4a911c2996fd0528
Andry Pebrianto <andrypeb227@gmail.com>
2023-10-11 22:27:40 +0700

    Fix typo

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import Env from "./Infrastructures/utils/Env";
 PostgreDataSource.initialize()
   .then(async () => {
     createServer.listen(Env.PORT, () => {
-      console.log(`Server started on port ${Env.PORT} with ${process.env.NODE_ENV} environment`);
+      console.log(`Server started on port ${Env.PORT} with ${Env.NODE_ENV} environment`);
       console.log(`Visit http://localhost:${Env.PORT}`);
       console.log("Developed by Andry Pebrianto");
     });
