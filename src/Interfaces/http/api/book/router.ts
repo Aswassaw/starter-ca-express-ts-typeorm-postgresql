377d1aa252fffee980c2c26c44518111ddb0651f
Andry Pebrianto <andrypeb227@gmail.com>
2023-10-16 00:07:46 +0700

    Menyederhanakan penulisan validation

--- src/Interfaces/http/api/book/router.ts
+++ src/Interfaces/http/api/book/router.ts
@@ -1,23 +1,19 @@
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import BookService from "./service";
 import container from "../../../../Infrastructures/container";
-import { addBookSchema } from "./validation";
+import runValidation from "../../../../Infrastructures/validation/runValidation";
+import { addBookSchema } from "../../../../Infrastructures/validation/schema/book";
 
 const bookService: BookService = new BookService(container);
 
 const router: Router = Router();
 // POST | /book
-router.post("/book", (req: Request, res: Response) => {
-  const { error } = addBookSchema.validate(req.body);
-  if (error)
-    return res.status(400).json({
-      code: 400,
-      status: "failed",
-      message: "Validation Failed",
-      error: error.details[0].message,
-    });
-
-  bookService.addBookService(req, res);
-});
+router.post(
+  "/book",
+  (req: Request, res: Response, next: NextFunction) => {
+    runValidation(req, res, next, addBookSchema);
+  },
+  bookService.addBookService
+);
 
 export default router;
